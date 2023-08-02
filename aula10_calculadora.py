64b1bab2889260f10656790b5358e921b649d2b9
Marcelo Zanoni Camargo <134171136+marcelozc@users.noreply.github.com>
2023-08-01 22:33:36 -0300

    Add files via upload

---
+++ aula10_calculadora.py
@@ -0,0 +1,44 @@
+#Add
+def add(n1, n2):
+  return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+  return n1 * n2
+
+#Divide
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add, 
+  "-": subtract, 
+  "*": multiply, 
+  "/": divide 
+}
+
+def calculator():
+  num1 = float(input("What's the first number? "))
+  
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+  while should_continue:
+    operation_symbol = input("Pick a operation from the above: ")
+    num2 = float(input("What's the second number? "))
+    operation_function = operations[operation_symbol]
+    answer = operation_function(num1,num2)
+  
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+  
+    if input (f"Type 'y' to continue calculating with {answer}, or 'n' to start a new calculation: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
